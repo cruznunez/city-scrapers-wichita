bb44f11616978fa7b7ca1b5469af410d5e9b0116
Cruz Núñez <cruz@Cruzs-15-MacBook-Pro.local>
2024-04-05 11:00:36 -0400

    add sccab spider

---
+++ city_scrapers/spiders/wicks_sccab.py
@@ -0,0 +1,73 @@
+from urllib.parse import urljoin
+
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.items import Meeting
+from city_scrapers_core.spiders import CityScrapersSpider
+from dateutil.parser import parse
+import re
+
+
+class WicksSccabSpider(CityScrapersSpider):
+    name = "wicks_sccab"
+    agency = "Sedgwick County: Corrections Advisory Boards"
+    timezone = "America/Chicago"
+    start_urls = ["https://www.sedgwickcounty.org/corrections/corrections-advisory-boards/"]
+
+    def parse(self, response):
+        """
+        `parse` should always `yield` Meeting items.
+        """
+        start_time = '8 a.m.'
+        end_time = '9 a.m.'
+        location = {
+            "name": "Adult Field Services",
+            "address": "905 N. Main, Wichita, Kansas",
+        }
+        for item in response.css('h4:nth-of-type(2) + *').css('li'):
+            meeting = Meeting(
+                title="Community Corrections Advisory Board Monthly Meeting",
+                description="",
+                classification=BOARD,
+                start=self._parse_start(item, start_time),
+                end=self._parse_end(item, end_time),
+                all_day=False,
+                time_notes="",
+                location=location,
+                links=self._parse_links(response, item),
+                source=response.url,
+            )
+
+            meeting["status"] = self._get_status(meeting)
+            meeting["id"] = self._get_id(meeting)
+
+            yield meeting
+
+    def _parse_date(self, item):
+        """Parse date"""
+        date = re.search(r"\D+ \d+, \d+", item.css('::text').get()).group()
+        return date
+
+    def _parse_start(self, item, start_time):
+        """Parse start datetime as a naive datetime object."""
+        date = self._parse_date(item)
+        parsed_datetime = parse(f"{date} {start_time}")
+        return parsed_datetime
+
+    def _parse_end(self, item, end_time):
+        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
+        date = self._parse_date(item)
+        parsed_datetime = parse(f"{date} {end_time}")
+        return parsed_datetime
+
+    def _parse_links(self, response, item):
+        """Parse or generate links."""
+        output = []
+        links = item.css('a')
+        for link in links:
+            title = link.css('::text').get()
+            href = link.css('::attr(href)').get()
+            url = urljoin(response.url, href)
+
+            output.append({ "title": title, "href": url })
+
+        return output
---
+++ tests/test_wicks_sccab.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import BOARD
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.wicks_sccab import WicksSccabSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "wicks_sccab.html"),
+    url="https://www.sedgwickcounty.org/corrections/corrections-advisory-boards/",
+)
+spider = WicksSccabSpider()
+
+freezer = freeze_time("2024-04-01")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Community Corrections Advisory Board Monthly Meeting"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2024, 1, 11, 8, 0)
+
+
+def test_end():
+    assert parsed_items[0]["end"] == datetime(2024, 1, 11, 9, 0)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert parsed_items[0]["id"] == "wicks_sccab/202401110800/x/community_corrections_advisory_board_monthly_meeting"
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "Adult Field Services",
+        "address": "905 N. Main, Wichita, Kansas"
+    }
+
+
+def test_source():
+    assert parsed_items[0]["source"] == "https://www.sedgwickcounty.org/corrections/corrections-advisory-boards/"
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [{
+        'title': 'Agenda',
+        'href': 'https://www.sedgwickcounty.org/media/65739/01-11-24-agenda.docx'
+    },
+    {
+        'title': 'Minutes',
+        'href': 'https://www.sedgwickcounty.org/media/65966/1-11-24-minutes.docx',
+    }
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
